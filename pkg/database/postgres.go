bfbdf85b82fc3463dee79ad9b0665e4d10b85522
muktiabdii <abdisyukur10@gmail.com>
2026-05-13 19:28:30 +0700

    feat: add helper for postgres and s3

---
+++ pkg/database/postgres.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get sql.DB from gorm: %v", err)
+	}
+
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(10)
+
+	DB = db
+	log.Println("database connected successfully")
+}
---
+++ pkg/storage/s3.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"mime/multipart"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+var S3Client *s3.Client
+var BucketName string
+
+func Connect() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(os.Getenv("AWS_REGION")),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+			os.Getenv("AWS_ACCESS_KEY"),
+			os.Getenv("AWS_SECRET_KEY"),
+			"",
+		)),
+	)
+	if err != nil {
+		log.Fatalf("failed to load AWS config: %v", err)
+	}
+
+	S3Client = s3.NewFromConfig(cfg)
+	BucketName = os.Getenv("AWS_BUCKET")
+	log.Println("S3 connected successfully")
+}
+
+func UploadFile(ctx context.Context, key string, file multipart.File, contentType string) (string, error) {
+	_, err := S3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(BucketName),
+		Key:         aws.String(key),
+		Body:        file,
+		ContentType: aws.String(contentType),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to upload file to S3: %w", err)
+	}
+
+	// return path format: /uploads/pdf/<filename>
+	url := fmt.Sprintf("/uploads/pdf/%s", key)
+	return url, nil
+}
+
+func DeleteFile(ctx context.Context, key string) error {
+	_, err := S3Client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(BucketName),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete file from S3: %w", err)
+	}
+	return nil
+}
